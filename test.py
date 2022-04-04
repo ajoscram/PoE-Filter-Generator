a3c075847dcaae05be5e05227301a6620a2d1dbb
Alejandro Schmidt <ajoscram@gmail.com>
2022-04-03 19:13:06 -0600

    Removed test.py

--- test.py
+++
@@ -1,8 +0,0 @@
-import handlers.econ as econ
-import handlers.tag as tag
-from classes.rule import Rule
-from classes.section import Section
-
-description = "   10  "
-print(description.isdigit())
-print(int(description))
